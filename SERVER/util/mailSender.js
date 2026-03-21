d01920227a2cf0d7a0ff9f9959e406048c2b3a25
khandaitmk <manishkhandait05@gmail.com>
2026-03-22 01:01:12 +0530

    some mail changes

--- SERVER/util/mailSender.js
+++ SERVER/util/mailSender.js
@@ -3,14 +3,14 @@ const nodemailer = require("nodemailer");
 exports.mailSender = async (mail, title, body) => {
     try {
         const transporter = nodemailer.createTransport({
-            host: "smtp.resend.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: "resend",
-                pass: process.env.RESEND_API,
-            },
-        });
+    host: "smtp.resend.com",
+    port: 587,        // ✅ change from 465 to 587
+    secure: false,    // ✅ change from true to false
+    auth: {
+        user: "resend",
+        pass: process.env.RESEND_API,
+    },
+});
 
         let info = await transporter.sendMail({
             from: "onboarding@resend.dev",
