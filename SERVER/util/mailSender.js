b3f833cc00799d7f274d6ee18581eaeee94a4d62
khandaitmk <manishkhandait05@gmail.com>
2026-03-22 01:26:40 +0530

    using bravoo

--- SERVER/util/mailSender.js
+++ SERVER/util/mailSender.js
@@ -1,14 +1,14 @@
-const Brevo = require("@getbrevo/brevo");
+const { ApiClient, TransactionalEmailsApi, SendSmtpEmail } = require("@getbrevo/brevo");
 
-const client = Brevo.ApiClient.instance;
+const client = ApiClient.instance;
 const apiKey = client.authentications["api-key"];
 apiKey.apiKey = process.env.BREVO_API_KEY;
 
-const transactionalEmailsApi = new Brevo.TransactionalEmailsApi();
+const transactionalEmailsApi = new TransactionalEmailsApi();
 
 exports.mailSender = async (mail, title, body) => {
     try {
-        const sendSmtpEmail = new Brevo.SendSmtpEmail();
+        const sendSmtpEmail = new SendSmtpEmail();
         sendSmtpEmail.to = [{ email: mail }];
         sendSmtpEmail.sender = { email: "manishkhandait05@gmail.com", name: "Eduverse" };
         sendSmtpEmail.subject = title;
