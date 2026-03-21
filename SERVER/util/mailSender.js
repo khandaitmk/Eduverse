97e17173ce3b9c4f5d0dc6e065538dbed032b991
khandaitmk <manishkhandait05@gmail.com>
2026-03-22 00:38:24 +0530

    mail port changed

--- SERVER/util/mailSender.js
+++ SERVER/util/mailSender.js
@@ -4,8 +4,8 @@ exports.mailSender = async (mail, title, body) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
-      port: 465, // ✅ changed from 587
-      secure: true, // ✅ changed from false
+      port: 587, // ✅ changed from 587
+      secure: false, // ✅ changed from false
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
